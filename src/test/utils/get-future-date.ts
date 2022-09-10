d863af016d645dbce73cae0ff815638155fb2c70
Mauricio Cardoso <mauricio.cds00@gmail.com>
2022-09-10 18:15:20 -0300

    utils de data

--- src/test/utils/get-future-date.ts
+++ src/test/utils/get-future-date.ts
@@ -1,8 +1,10 @@
-import { setYear } from "date-fns";
+import { parseISO, setYear } from "date-fns";
 
 /**
  * Função para retornar uma data com um ano para frente
  * @param date Data
  * @returns Data com um ano a mais
  */
-export function setNextYear(date: string) {}
+export function getFutureDate(date: string): Date {
+  return setYear(parseISO(date), new Date().getFullYear() + 1);
+}
